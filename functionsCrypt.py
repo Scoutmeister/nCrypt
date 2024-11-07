d3dc37eadc88520bdabafebd71efc7a05e6e0b33
Hugo Polstam <hugo.polstam@gmail.com>
2024-11-07 17:49:38 +0100

    fixat så att programmet kan kryptera alla typer av filer(jag är fucking bäst)

--- functionsCrypt.py
+++ functionsCrypt.py
@@ -62,15 +62,15 @@ def decrypt_file(filename):
     try:
         # Reads the content of the file and saves it
         with open(filename, "rb") as file:
-            cipher_text = file.read()
+            cipher_text = file.readlines()
 
         # Creates name for the new decrypted file
-        decryptedFileName = filename.split(".")[0] + ".txt"
+        decryptedFileName = cipher_suite.decrypt(cipher_text[1].decode())
         os.remove(filename)
 
         # Decrypts the ciphertext and replace it with plaintext
         with open(decryptedFileName, "wb") as file:
-            plaintext = cipher_suite.decrypt(cipher_text)
+            plaintext = cipher_suite.decrypt(cipher_text[0].strip())
             file.write(plaintext)
 
         print(color_green(f"The file '{filename}' has been decrypted to: {decryptedFileName}"))
